723e8ef0fc07d1682be195d97c5705e91040620b
Kryštof <krystof.kratky2003@gmail.com>
2022-01-28 21:40:31 +0000

    update

--- biketower/main.js
+++ biketower/main.js
@@ -1,24 +1,26 @@
 const puppeteer = require("puppeteer");
 var fs = require("fs");
 
-var stream = fs.createWriteStream("data/data.csv", { flags: "a" });
+const dataLocation = "../data/biketower/data.csv";
+
+var stream = fs.createWriteStream(dataLocation, { flags: "a" }); // create a writable stream
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-  });
-  const page = await browser.newPage();
+  }); // launch browser 
+  const page = await browser.newPage(); // create new page
 
   await page.goto("https://www.pardubice.eu/biketower/", {
     waitUntil: "networkidle2",
-  });
+  }); // go to page
 
   content = await page.evaluate(() => {
     const firstLine = document
       .querySelector(
         "body > div.main > div > div.row > div > div > div > div > ul > li:nth-child(1)"
       )
-      .innerText.split(":");
+      .innerText.split(":");  
     const secondLine = document
       .querySelector(
         "body > div.main > div > div.row > div > div > div > div > ul > li:nth-child(2)"
@@ -30,10 +32,10 @@ var stream = fs.createWriteStream("data/data.csv", { flags: "a" });
       )
       .innerText.split(":");
     datetime = datetime[1].substring(1) + ":" + datetime[2];
-    return `\n${datetime},${firstLine[1].replaceAll(
+    return `${datetime},${firstLine[1].replaceAll(
       " ",
       ""
-    )},${secondLine[1].replaceAll(" ", "")}`;
+    )},${secondLine[1].replaceAll(" ", "")}\n`;
   });
   await browser.close();
   stream.write(content);
--- chrudimka/main.js
+++ chrudimka/main.js
@@ -1,47 +1,64 @@
 const puppeteer = require("puppeteer");
 var fs = require("fs");
 
-var stream = fs.createWriteStream("data/data.csv", { flags: "a" });
+const dataLocation = "../data/chrudimka/data.csv";
 
+var stream = fs.createWriteStream(dataLocation, { flags: "a" }); // create a writable stream
+const data = fs.readFileSync(dataLocation, "utf8"); // read file
+const lastRecord = data.split("\n").reverse()[0].split(",")[0]; // get timestamp of last record
+
+// main function
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-  });
+  }); // launch browser
   const page = await browser.newPage();
 
   // wait until the page is loaded
-  await page.goto(  
+  await page.goto(
     "https://hydro.chmi.cz/hpps/popup_hpps_prfdyn.php?seq=307210",
     {
       waitUntil: "networkidle2",
     }
-  );
+  ); // go to page
 
-  content = await page.evaluate(() => {
-    // get date and time of record from table
-    const datetime = document.querySelector(
-      "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(1)"
-    ).innerText;
-    // get status from table
-    const status = document.querySelector(
-      "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(2)"
-    ).innerText;
-    // get flow from table
-    let flow = document.querySelector(
-      "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(3)"
-    ).innerText;
-    // get tempererature from table
-    const temperature = document.querySelector(
-      "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(4)"
-    ).innerText;
-    // check if temperature is filled in
-    if (temperature != "")
-      return `\n${datetime},${status},${flow},${temperature}`;
-    // if temperature is not filled in return empty string
-    else return "";
-  });
-  await browser.close();
-  // add data to csv
-  if(content != "") stream.write(content);
-  stream.end();
+  content = await page.evaluate(
+    (lastRecord) => {
+      const datetime = document.querySelector(
+        "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(1)"
+      ).innerText; // get date and time of record from table
+
+      const status = document.querySelector(
+        "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(2)"
+      ).innerText; // get status from table
+
+      let flow = document.querySelector(
+        "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(3)"
+      ).innerText; // get flow from table
+
+      const temperature = document.querySelector(
+        "#page > div.box > div > table.stdstationtbl > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(11) > td:nth-child(4)"
+      ).innerText; // get tempererature from table
+
+      // check if temperature is filled in
+      if (temperature != "" && lastRecord != datetime) {
+        // if temperature is filled in and timestamp is not the same as in last record
+        return `${datetime},${status},${flow},${temperature}\n`; // format data for csv
+      } else {
+        // if temperature is not temperature in the record or there is no new data, return null
+        return null;
+      }
+    },
+    [lastRecord]
+  );
+  await browser.close(); // close browser
+
+  if (content == null) {
+    // if there is no new data
+    console.log("no new data🐿️");
+  } else {
+    // if there is new data write them to file
+    stream.write(content);
+  }
+  stream.end(); // close file
 })();
